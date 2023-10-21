90bc9506317c1a7918027bd53aa019906652a707
IsuruMadushan97 <tm.isurumadushan@gmail.com>
2023-10-21 21:50:08 +1100

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -5,94 +5,94 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("SEF App test");
 
+        //Valid artist information
         Artist artist = new Artist(
-                "668MMMRR%%",
-                "John Doe",
+                "655GHIJK@$",
+                "Alicia Davis",
                 "Melbourne|Victoria|Australia",
-                "05-11-1980",
-                "A talented artist tes this functions if this workd shei i iwe ididid ii",
-                new ArrayList<>(List.of("Sculptor")),
+                "05-11-1972",
+                "R&B and soul singer known for powerful vocals.",
+                new ArrayList<>(List.of("Singer", "Songwriter")),
                 new ArrayList<>(List.of("Abstract", "Realism")),
-                new ArrayList<>(List.of("2023, Best Song Written For Visual Media"))
+                new ArrayList<>(List.of("2019, Best R&B Album"))
         );
 
-        // User Sample 2
-        /* Artist artist1 = new Artist(
-                "123ABC!",
+        //Invalid artist ID
+         /*Artist artist1 = new Artist(
+                "123InvalidID", //Invalid ID
                 "Alice Smith",
-                "123 Elm St",
+                "987 Lamest St",
                 "01-01-1995",
-                "A versatile artist with a passion for painting.",
-                new ArrayList<>(List.of("Painter", "Sculptor")),
-                new ArrayList<>(List.of("Abstract", "Realism")),
-                new ArrayList<>(List.of("2019, Best Painter"))
+                "A versatile artist with a passion for singing.",
+                new ArrayList<>(List.of("Singer")),
+                new ArrayList<>(List.of("Producer", "DJ")),
+                new ArrayList<>(List.of("2019, Best singer"))
         );*/
 
-        // User Sample 3
-        /* Artist artist2 = new Artist(
-                "456DEF@",
+         //Invalid Genres (Pop and Rock Together)
+         /*Artist artist2 = new Artist(
+                "668MMMRR%%",
                 "Bob Johnson",
-                "456 Oak St",
-                "31-02-2000", // Invalid birthdate
-                "An artist with a passion for sculpture.",
-                new ArrayList<>(List.of("Sculptor")),
-                new ArrayList<>(List.of("Surrealism")),
-                new ArrayList<>(List.of("2021, Sculptor of the Year"))
+                "749 Occenls St",
+                "28-02-1980",
+                "Singer and songwriter with a passion for music.",
+                new ArrayList<>(List.of("Lyrics Artist", "Songwriter")),
+                new ArrayList<>(List.of("Pop", "Rock")),// Invalid genre constraint
+                new ArrayList<>(List.of("2021, Best Song Written For Visual Media"))
         );*/
 
-        // User Sample 4
+        // Invalid Bio (Less than 10 words)
         /*Artist artist3 = new Artist(
-                "789GHI#",
+                "569MMMRR_%",
                 "Catherine Davis",
-                "789 Pine St",
-                "15-07-1992",
+                "789 epping St",
+                "15-03-1992",
                 "A talented artist specializing in abstract art.",
-                new ArrayList<>(List.of("Painter")),
-                new ArrayList<>(List.of("Abstract")),
-                new ArrayList<>(List.of("2018, Best Abstract Artist"))
+                new ArrayList<>(List.of("Singer")),
+                new ArrayList<>(List.of("Jazz")),
+                new ArrayList<>(List.of("2021, singer of the Year"))
         );*/
 
-        // User Sample 4
-        /*
-        Artist artist4 = new Artist(
-                "ABC456%",
+        // Invalid bio length
+        /*Artist artist4 = new Artist(
+                "569MMMRR_%",
                 "David Wilson",
-                "101 Maple St",
-                "02-05-1988",
+                "838 kingsburry St",
+                "12-11-1992",
                 "A short bio.", // Invalid bio length
                 new ArrayList<>(List.of("Painter")),
                 new ArrayList<>(List.of("Impressionism")),
                 new ArrayList<>(List.of("2017, Best Impressionist Painter"))
         );*/
 
-        // User Sample 5
+        // Valid Artist Update with Same Birth Year
         /*Artist artist5 = new Artist(
-                "XYZ123&",
-                "Eva Anderson",
-                "222 Cedar St",
-                "10-12-1990",
+                "569MMMRR_%",
+                "Emily Johnson",
+                "386 dear park St",
+                "18-10-1993",
                 "A talented artist with a bio.",
-                new ArrayList<>(List.of("Sculptor")),
-                new ArrayList<>(List.of("Surrealism")),
-                new ArrayList<>(List.of("Best Sculptor 2023")) // Invalid award format
+                new ArrayList<>(List.of("Singer", "Songwriter")),
+                new ArrayList<>(List.of("Rock", "Jazz")),
+                new ArrayList<>(List.of("2022, Updated Award")) // Invalid award format
         );*/
 
-        // User Sample 6
+        // Invalid Artist Update (Occupations Changed for Artist Born Before 2000)
         /*Artist artist6 = new Artist(
-                "LMN789$",
-                "Frank Johnson",
-                "333 Walnut St",
-                "20-08-1995",
-                "An artist with a bio.",
-                new ArrayList<>(List.of("Painter")),
-                new ArrayList<>(List.of("Pop", "Rock")), // Invalid genre constraint
+                "723XYZPP!@",
+                "Alicia Davis",
+                "681 planty St",
+                "12-04-1984",
+                "Versatile musician and performer.",
+                new ArrayList<>(List.of("Painter")),// Invalid genre constraint
+                new ArrayList<>(List.of("Pop", "Rock")),
                 new ArrayList<>(List.of("2020, Best Pop Artist"))
         );*/
 
 
-        //artist.addArtist();
-        artist.updateArtist();
+        artist.addArtist();
+        //artist.updateArtist();
     }
 }
